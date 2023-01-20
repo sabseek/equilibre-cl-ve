06cdb42a7fe87af7748f7b7fcf254eff8e8de658
wendel <19378023+bitdeep@users.noreply.github.com>
2023-01-20 18:48:06 -0300

    voter scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,11 +4,60 @@ import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-preprocessor";
 import "hardhat-abi-exporter";
-
 import { resolve } from "path";
-
 import { config as dotenvConfig } from "dotenv";
 import { HardhatUserConfig, task } from "hardhat/config";
+import fs from "fs";
+
+async function loadCfg(){
+  const contracts = JSON.parse(fs.readFileSync('../contracts.json').toString());
+  const network = await hre.ethers.provider.getNetwork();
+  if( ! contracts ){
+    new Error(`contracts.json is empty.`);
+  }
+  const r = contracts[network.chainId];
+  if( ! r ){
+    new Error(`No contracts for chain: ${network.chainId}`);
+  }
+  const [owner] = await hre.ethers.getSigners();
+  console.log(`Account: ${owner.address}, Network: ${network.chainId}.`);
+  return r;
+}
+
+task("distro", "Voter.distro").setAction(async () => {
+  const cfg = await loadCfg();
+  const Main = await hre.ethers.getContractFactory("Voter")
+  const main = Main.attach(cfg.Voter);
+  const tx = await main.distro();
+  console.log(`${tx.hash}`);
+});
+
+task("updateAll", "Voter.updateAll").setAction(async () => {
+  const cfg = await loadCfg();
+  const Main = await hre.ethers.getContractFactory("Voter")
+  const main = Main.attach(cfg.Voter);
+  const tx = await main.updateAll();
+  console.log(`${tx.hash}`);
+});
+
+task("distributeFees", "Voter.distributeFees").setAction(async () => {
+  const cfg = await loadCfg();
+  const Main = await ethers.getContractFactory("Voter")
+  const main = Main.attach(cfg.Voter);
+  let pools = [], gauges = [];
+  const length = await main.length();
+  for( let i = 0 ; i < length ; ++ i ){
+    const pool = await main.pools(i);
+    const gauge = await main.gauges(pool);
+    console.log(` - ${i}: ${pool} = ${gauge}`);
+    pools.push( pool );
+    gauges.push( gauge );
+  }
+  let tx = await main.distributeFees(gauges);
+  console.log(`${tx.hash}`);
+});
+
+
 
 dotenvConfig({ path: resolve(__dirname, "./.env") });
 
@@ -19,11 +68,11 @@ const config: HardhatUserConfig = {
     },
     mainnet: {
       url: "https://evm.kava.io",
-      accounts: [process.env.PRIVATE_KEY_ADMIN!]
+      accounts: [process.env.PRIVATE_KEY!]
     },
     testnet: {
       url: "https://evm.testnet.kava.io",
-      accounts: [process.env.PRIVATE_KEY_ADMIN!]
+      accounts: [process.env.PRIVATE_KEY!]
     },
   },
   solidity: {
@@ -60,5 +109,6 @@ const config: HardhatUserConfig = {
     ]
   }
 };
+
 
 export default config;
